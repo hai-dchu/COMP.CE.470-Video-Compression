c75072b471df984deda1339a6d9040b1694f625f
Hai Chu <duchaichu1303@gmail.com>
2026-01-29 12:23:50 +0200

    Add shift left, update shift right, change_precision

--- exercise1/main.cpp
+++ exercise1/main.cpp
@@ -69,15 +69,15 @@ class FixedPoint {
     }
 
     FixedPoint operator+(const FixedPoint& other) const {
-        if ((-2 >> 1) < 0) { // Arithmetic Shift Right (ASR)
-            // Decide the smaller and larger values 
+        if ((-2 >> 1) < 0) {  // Arithmetic Shift Right (ASR)
+            // Decide the smaller and larger values
             int first = m_decimal_digits <= other.m_decimal_digits ? m_decimal_digits : other.m_decimal_digits;
             int second = m_decimal_digits > other.m_decimal_digits ? m_decimal_digits : other.m_decimal_digits;
             int value_first = m_decimal_digits <= other.m_decimal_digits ? m_value : other.m_value;
             int value_second = m_decimal_digits > other.m_decimal_digits ? m_value : other.m_value;
             int bit_count_first = m_decimal_digits <= other.m_decimal_digits ? bit_count : other.bit_count;
-            T max = 1 << (bit_count_first - 2); // second most significant bit for type T
-            
+            T max = 1 << (bit_count_first - 2);  // second most significant bit for type T
+
             // In case the two values have different decimal digits,
             // the smaller will be shifted right,
             // while the larger be shifted left
@@ -85,13 +85,13 @@ class FixedPoint {
             while (first != second && !(value_first & max)) {
                 // !(value_first & max) decide if the last non-sign bit of value_first
                 // is 0 or 1. If 1 then it is not suitable for left shift anymore
-                first ++;
+                first++;
                 value_first <<= 1;
             }
             while (first != second && !(value_second & 1)) {
                 // !(value_second & 1) decide if the first bit of value_second
                 // is 0 or 1. If 1 then it is not suitable for left shift anymore
-                second --;
+                second--;
                 value_second >>= 1;
             }
 
@@ -99,7 +99,7 @@ class FixedPoint {
                 // cannot shift either value_first left or value_second right
                 // anymore without changing they original values
                 // TODO
-                std::cout<<"Cannot balance decimal digits of two numbers"<<std::endl;
+                std::cout << "Cannot balance decimal digits of two numbers" << std::endl;
                 return FixedPoint(0, 0);
             }
 
@@ -120,19 +120,27 @@ class FixedPoint {
     }
 
     FixedPoint operator<<(int shift) const {
-        return FixedPoint(0,0);
+        int new_value = m_value << shift;
+        T max = 1 << (bit_count - 2);  // second most significant bit for type T
+        int new_decimal_digits = m_decimal_digits + shift <= max ? m_decimal_digits + shift : max;
+        return FixedPoint(new_value, new_decimal_digits);
     }
 
     FixedPoint operator>>(int shift) const {
-        int new_value = m_value >> (shift - 1);
+        int new_value = (m_value + (1 << shift - 1)) >> shift;
         int new_decimal_digits = m_decimal_digits >= shift ? m_decimal_digits - shift : 0;
         return FixedPoint(new_value, new_decimal_digits);
     }
 
     [[nodiscard]] FixedPoint change_precision(int new_decimal_digits) const {
-        int tmp_decimal_digits = new_decimal_digits >= 1 ? new_decimal_digits - (m_decimal_digits != 0) : 0;
-        int new_m_value = m_value << tmp_decimal_digits;
-        return FixedPoint(new_m_value, new_decimal_digits);
+        if (new_decimal_digits > m_decimal_digits) {
+            // shift left
+            return FixedPoint(m_value, m_decimal_digits) << (new_decimal_digits - m_decimal_digits);
+        } else {
+            // shift right
+            return FixedPoint(m_value, m_decimal_digits) >> (m_decimal_digits - new_decimal_digits);
+        }
+        return FixedPoint(0, 0);
     }
 
    private:
@@ -197,6 +205,10 @@ void test() {
     compare(c, "-0.03125");
     c = b * a;
     compare(c, "-0.03125");
+
+    c = FixedPoint(16, 5);
+    c = c >> 4;
+    compare(c, "0.5");
 
     /* Uncomment if doing the extra part
     c = a / b;
